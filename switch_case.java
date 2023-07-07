fa7b7750c1c5dbbbb601d3979d29b22345bdddb6
Tabish Raza <tabishraza586@gmail.com>
2023-07-08 00:03:24 +0530

    modified the code with enhance switch case

--- switch_case.java
+++ switch_case.java
@@ -5,31 +5,15 @@ public class switch_case {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the day");
         int day = input.nextInt();
-        switch (day) {
-            case 1:
-                System.out.println("monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Please enter valid day");
-                break;
+         switch (day) {
+            case 1 -> System.out.println("monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Please enter valid day");
         }
     }    
 
